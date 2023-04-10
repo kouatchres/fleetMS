a8cf5f3567f1a9c954fd363094caf9f7ad7fafb6
kouatchoua mark <kouatchres@gmail.com>
2023-04-10 13:45:39 +0100

    first all

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Country value(1, 'Yaounde', 'Cameroon', 'Africa', 'Africa in miniature','Cameroonian');
+INSERT INTO Country value(2, 'Abuja', 'Nigeria', 'Africa', 'Africa most populous country','Nigerian'),
+INSERT INTO Country value(3, 'Washington DC', 'USA', 'North America', 'most powerful contry in the world','American'),
+INSERT INTO Country value(4, 'Alberta', 'Canada', 'North America', 'Largest ice country','Canadian'),
+INSERT INTO Country value(5, 'Beijing', 'China', 'Asia', 'Worlds factory','Chinese'),
+INSERT INTO Country value(6, 'New Delhi', 'India', 'Asia', 'Worlds most populous country','Indian');
